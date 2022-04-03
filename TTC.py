a7b94cd18536f9b9bcf01bc0d0ce62460befc3f5
Jelloface <100376987+Jelloooface@users.noreply.github.com>
2022-04-03 16:02:17 -0500

    Create TTC.py

---
+++ TTC.py
@@ -0,0 +1,34 @@
+import random
+
+choices = ["Yes","No","Maybe","Uno Reverse","No u"]
+
+computer = random.choice(choices)
+computer1 = random.choice(choices)
+computer2 = random.choice(choices)
+computer3 = random.choice(choices)
+computer4 = random.choice(choices)
+computer5 = random.choice(choices)
+computer6 = random.choice(choices)
+computer7 = random.choice(choices)
+player = None
+
+
+print("Hello I am the talking CHIP from the Lay Down potato chip co. I have wisdom and I will answer any questions you have ")
+player = input("What is youre widom MORTAL?: ")
+print("CHIP: ",computer)
+player = input("Any other questions?: ")
+print("CHIP: ",computer1)
+player = input("Any other questions?: ")
+print("CHIP: ",computer2)
+player = input("Any other questions?: ")
+print("CHIP: ",computer3)
+player = input("Any other questions?: ")
+print("CHIP: ",computer4)
+player = input("Any other questions?: ")
+print("CHIP: ",computer5)
+player = input("Any other questions?: ")
+print("CHIP: ",computer6)
+player = input("Any other questions?: ")
+print("CHIP: ",computer7)
+
+  
